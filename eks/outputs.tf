7dfd4abb7a1135da026cd2e2df7af871f76890b5
Kauan Silva de Sousa <kauanssousa@gmail.com>
2025-02-09 14:55:39 -0300

    Update outputs.tf

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,7 +1,7 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
-}
+#output "eks_cluster_name" {
+#  value = aws_eks_cluster.eks_cluster.name
+#}
 
-output "eks_node_group_name" {
-  value = aws_eks_node_group.eks_node_group.name
-}
+#output "eks_node_group_name" {
+#  value = aws_eks_node_group.eks_node_group.name
+#}
